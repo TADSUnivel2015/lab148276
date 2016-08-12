3d7b8f3f340184ac55f0f112d537293a04fd86dd
Alex Tezza <Alex Tezza@DESKTOP-F4OTEBU>
2016-08-11 22:00:30 -0300

    #4 Criação dos EJBs ProcessEntrega e ProcessVenda.

---
+++ lab148276/src/main/java/br/tezza/ejb/ProcessEntrega.java
@@ -0,0 +1,14 @@
+package br.tezza.ejb;
+
+import javax.ejb.Stateless;
+
+import br.tezza.pojo.PojoEntrega;
+
+@Stateless
+public class ProcessEntrega {
+
+    public void processarEntrega (PojoEntrega entrega) {
+
+    }
+
+}
---
+++ lab148276/src/main/java/br/tezza/ejb/ProcessVenda.java
@@ -0,0 +1,14 @@
+package br.tezza.ejb;
+
+import javax.ejb.Stateless;
+
+import br.tezza.pojo.PojoVenda;
+
+@Stateless
+public class ProcessVenda {
+
+    public void processarVenda(PojoVenda venda) {
+
+    }
+
+}
